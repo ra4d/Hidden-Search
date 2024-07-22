d230c9ee0bdbabecdd91a32125a3d107c9a198ec
ra4d <122443633+ra4d@users.noreply.github.com>
2024-07-22 06:32:05 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+const inputSearch = document.querySelector('.inputSearch')
+const btn = document.querySelector('.btn')
+
+
+btn.addEventListener("click" , ()=>{
+    inputSearch.classList.toggle("active");
+})
